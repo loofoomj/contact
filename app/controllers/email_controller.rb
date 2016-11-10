2c7cbdb896eb049e2b03bbfb86576e27f370b52a
loofoomj@naver.com <ec2-user@ip-172-31-26-24.ap-northeast-1.compute.internal>
2016-11-10 22:19:46 +0000

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,20 @@ class EmailController < ApplicationController
 	end
 
 	def email_send
-		@email = params[:email]
-		@content = params[:content]
+		email = params[:email]
+		title = params[:title]
+		content = params[:content]
+
+		gmail = Gmail.connect("boognew","whtpehd0927!")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body content
+			end
+		end
+
+		gmail.logout
+
 	end
 end
